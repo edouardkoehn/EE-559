0a3ef6974893ff00696b1f4dbe88fef2ff9e8691
Edouard Koehn <edouard.koehn@epfl.ch>
2024-04-22 11:34:27 +0200

    first implmentation of the pipeline

---
+++ src/dataset.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import torch
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+
+
+class CustomDataset(Dataset):
+    """Custom dataset for the MMHS150 dataset"""
+
+    def __init__(self, csv_file, img_dir, split, transform=None):
+        """__init__ function for CustomDataset
+
+        Args:
+            csv_file (str): Path to the csv file containing the dataset information
+            img_dir (str): Path to the directory containing the images
+            split (str): 'train' or 'test' or 'val'
+            transform (callable, optional): Optional transform to be applied on a sample
+        """
+        self.dataset = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.split = split
+        self.transform = transform
+
+        # Number of images in the dataset
+        self.dataset_len = len(self.dataset)
+
+    def __len__(self):
+        return self.dataset_len
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        image_index = self.dataset["index"].values[idx]
+
+        img_path = self.img_dir + str(image_index) + ".jpg"
+        image = Image.open(img_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        label = self.dataset[self.dataset["index"] == image_index][
+            "binary_hate"
+        ].values[0]
+        tweet_text = self.dataset[self.dataset["index"] == image_index][
+            "tweet_text_clean"
+        ].values[0]
+        img_text = self.dataset[self.dataset["index"] == image_index][
+            "img_text"
+        ].values[0]
+
+        sample = {
+            "image": image,
+            "label": label,
+            "tweet_text": tweet_text,
+            "img_text": img_text,
+        }
+
+        return sample
---
+++ src/models.py
@@ -0,0 +1,62 @@
+import os
+
+import torch
+from open_flamingo import create_model_and_transforms
+from torch import nn
+
+from src.utils import ROOT_DIR, load_config_model
+
+
+class Flamingo0S(nn.Module):
+    """Class for Flamingo with zero-shot learning"""
+
+    def __init__(self, config_path: os.path) -> None:
+        super(Flamingo0S, self).__init__()
+        config = load_config_model(config_path)
+        self.model_name = config["model_name"]
+
+        LANG_MODEL_PATH = os.path.join(
+            ROOT_DIR, "data", "pretrained_models", config["language_model"]
+        )
+        CACHE_MODEL = os.path.join(ROOT_DIR, "data", "pretrained_models")
+        FLAMINGO_MODEL_PATH = os.path.join(
+            ROOT_DIR,
+            "data",
+            "pretrained_models",
+            "OpenFlamingo-3B-vitl-mpt1b",
+            "checkpoint.pt",
+        )
+
+        self.model, self.image_processor, self.tokenizer = create_model_and_transforms(
+            clip_vision_encoder_path=config["vision_model"],
+            clip_vision_encoder_pretrained="openai",
+            lang_encoder_path=LANG_MODEL_PATH,
+            tokenizer_path=LANG_MODEL_PATH,
+            cross_attn_every_n_layers=config["n_cros_attention"],
+            cache_dir=CACHE_MODEL,
+        )
+        self.model.load_state_dict(torch.load(FLAMINGO_MODEL_PATH), strict=False)
+        self.model.eval()
+        self.tokenizer.padding_side = "left"
+
+        self.prompt_text = config["prompting"]
+
+    def forward(self, x, train=False):
+        """Method get the inferance from the model
+        Args : x(dict): containing the keys:image, labels, tweet_text, img_text
+        """
+        visual_input = [self.image_processor(x["image"])]
+        visual_input = torch.cat(visual_input, dim=0)
+        visual_input = visual_input.unsqueeze(1).unsqueeze(0)
+
+        text_input = [self.prompt_text]
+        text_input = self.tokenizer(text_input, return_tensors="pt")
+
+        generated_text = self.model.generate(
+            vision_x=visual_input,
+            lang_x=text_input["input_ids"],
+            attention_mask=text_input["attention_mask"],
+            max_new_tokens=20,
+            num_beams=3,
+        )
+        return {"generation": self.tokenizer.decode(generated_text[0])}
