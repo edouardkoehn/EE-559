bdc956558cde08679f927ee5700d01e241826b98
Edouard Erwan Koehn <koehn@izar1.izar.cluster>
2024-04-23 09:01:38 +0200

    fixing the pyproject.toml file

--- src/eval.py
+++ src/eval.py
@@ -1,9 +1,10 @@
 import json
 import os
-
+import time
 from src.dataset import CustomDataset
 from src.models import Flamingo0S
 from src.utils import ROOT_DIR
+print(time.time())
 
 dataset = CustomDataset(
     csv_file=os.path.join(ROOT_DIR, "data", "MMHS150K", "MMHS150K_text_in_image.csv"),
@@ -25,3 +26,5 @@ for i in range(10):
 res_path = os.path.join(ROOT_DIR, "data_test.json")
 with open(res_path, "w") as f:
     json.dump(res_dict, f)
+
+print(time.time())
