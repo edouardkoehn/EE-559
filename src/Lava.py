ba8f06df2e64651b56d83543210db18970836438
Edouard Erwan Koehn <koehn@izar1.izar.cluster>
2024-05-06 22:00:36 +0200

    Feat: implemntation of the evaluation loop

--- src/Lava.py
+++ src/Lava.py
@@ -1,21 +1,24 @@
+import json
+import os
+
 from src.dataset import CustomDataset
 from src.models import Lava
 from src.utils import ROOT_DIR
-import os
-
 
 # Load the dataset
 dataset = CustomDataset(
     csv_file=os.path.join(ROOT_DIR, "data", "MMHS150K", "MMHS150K_text_in_image.csv"),
     img_dir=os.path.join(ROOT_DIR, "data", "MMHS150K", "img_resized/"),
-    split="test"
+    split="test",
 )
 
-model=Lava(os.path.join(ROOT_DIR, "data",'config', 'config_Lava0S.json'))
+model = Lava(os.path.join(ROOT_DIR, "data", "config", "config_Lava0S.json"))
 
-output={}
-for i in range(0,100):
-    output[dataset[i]['index']]=output.append(model(dataset[i]))
+output = {}
+for i in range(0, 2):
+    print(model(dataset[i]))
+    output[dataset[i]["index"]] = model(dataset[i])
+    print(output)
 
-with open('results.json','wd') as f:
+with open("results.json", "wd") as f:
     json.dump(output, f)
