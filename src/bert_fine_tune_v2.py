0d412f3dee4c539ad257f3581edf5f3f34bbab30
Barbara Joanne de Groot <bdegroot@izar1.izar.cluster>
2024-05-17 13:51:34 +0200

    bert fine tune script debugged + bert done

--- src/bert_fine_tune_v2.py
+++ src/bert_fine_tune_v2.py
@@ -7,15 +7,21 @@ from torch.optim import AdamW
 from transformers import get_scheduler
 from tqdm.auto import tqdm
 import evaluate
+import json
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 # loading dataset
 tweets = pd.read_csv("data/MMHS150K/MMHS150K_with_img_text.csv")
-data = tweets[["tweet_text_clean", "binary_hate", "split"]]
+data = tweets[["tweet_text_clean", "binary_hate", "split", "index"]]
 data = data.rename(
-    columns={"tweet_text_clean": "text", "binary_hate": "labels", "split": "split"}
+    columns={
+        "tweet_text_clean": "text",
+        "binary_hate": "labels",
+        "split": "split",
+        "index": "index",
+    }
 )
 
 # Convert the pandas DataFrames to Hugging Face Datasets
@@ -28,18 +34,19 @@ dataset_dict = DatasetDict(
     {"train": train_dataset, "test": test_dataset, "validation": val_dataset}
 )
 
-# load model and tokenizer
-def load_pretrained(model_name):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-    if torch.cuda.is_available():
-        model = model.to(device)
-    return tokenizer, model
-
+# map for expected ids to labels
+id2label = {0: "Not hateful", 1: "Hateful"}
+label2id = {"Not hateful": 0, "Hateful": 1}
 
 # load bert
-tokenizer, model = load_pretrained("distilbert-base-cased")
+model_name = "bert-base-cased"  #'google/bert_uncased_L-4_H-128_A-2'
+tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=512)
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_name, num_labels=2, id2label=id2label, label2id=label2id
+)
+
+if torch.cuda.is_available():
+    model = model.to(device)
 
 
 def tokenize_function(examples):
@@ -48,17 +55,20 @@ def tokenize_function(examples):
 
 tokenized_data = dataset_dict.map(tokenize_function, batched=True)
 
+# Remove the text column because the model does not accept raw text as an input
+tokenized_data = tokenized_data.remove_columns(["text", "__index_level_0__", "split"])
+
 # Set the format of the dataset to return PyTorch tensors instead of lists
 tokenized_data.set_format("torch")
 
+# create a DataLoader for your training and test datasets so you can iterate over batches of data:
 train_dataloader = DataLoader(tokenized_data["train"], shuffle=True, batch_size=8)
 eval_dataloader = DataLoader(tokenized_data["test"], batch_size=8)
 
-optimizer = AdamW(model.parameters(), lr=5e-5)
+optimizer = AdamW(model.parameters(), lr=4e-5)
 
 num_epochs = 2
 num_training_steps = num_epochs * len(train_dataloader)
-# feel free to experiment with different num_warmup_steps
 lr_scheduler = get_scheduler(
     name="linear",
     optimizer=optimizer,
@@ -68,13 +78,14 @@ lr_scheduler = get_scheduler(
 
 progress_bar = tqdm(range(num_training_steps))
 
-# put the model in train mode
+# training the model
 model.train()
 
 # iterate over epochs
 for epoch in range(num_epochs):
     # iterate over batches in training set
     for batch in train_dataloader:
+        batch.pop("index")  # remove 'index' from batch
         batch = {k: v.to(device) for k, v in batch.items()}
         # **kwargs is a common idiom to allow an arbitrary number of arguments to functions
         # The **kwargs will give you all keyword arguments as a dictionary
@@ -108,14 +119,19 @@ for epoch in range(num_epochs):
 
         progress_bar.update(1)
 
+# evaluate model
 # define the metric you want to use to evaluate your model
 metric = evaluate.load("accuracy")
 progress_bar = tqdm(range(len(eval_dataloader)))
+
+predictions_bin_dict = {}
+predictions_prob_dict = {}
 
 # put the model in eval mode
 model.eval()
 # iterate over batches of evaluation dataset
 for batch in eval_dataloader:
+    indices = batch.pop("index")  # remove 'index' from batch and store it
     batch = {k: v.to(device) for k, v in batch.items()}
     with torch.no_grad():
         # pass the batches to the model and get the outputs
@@ -124,8 +140,21 @@ for batch in eval_dataloader:
     # get the logits from the outputs
     logits = outputs.logits
 
+    # use softmax to get the predicted probabilities
+    probabilities = torch.softmax(logits, dim=-1)
+
+    # add the probabilities of the positive class to the dictionary
+    for i, probability in zip(indices, probabilities[:, 0]):
+        predictions_prob_dict[
+            i.item()
+        ] = probability.item()  # assuming that the positive class is the second one
+
     # use argmax to get the predicted class
     predictions = torch.argmax(logits, dim=-1)
+
+    # add the predictions to the dictionary
+    for i, prediction in zip(indices, predictions):
+        predictions_bin_dict[i.item()] = prediction.item()
 
     # metric.add_batch() adds a batch of predictions and references
     # Metric.add_batch() by passing it your model predictions, and the references
@@ -134,3 +163,10 @@ for batch in eval_dataloader:
     progress_bar.update(1)
 # calculate a metric by  calling metric.compute()
 metric.compute()
+
+# save the predictions to a JSON file
+with open("predictions_bin_bert.json", "w") as f:
+    json.dump(predictions_bin_dict, f)
+
+with open("predictions_prob_bert.json", "w") as f:
+    json.dump(predictions_prob_dict, f)
